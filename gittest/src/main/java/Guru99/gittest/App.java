2ffdbdc183411192b5867126a42b8fe1fcab59b7
Sai <Sai@hi>
2017-04-02 16:00:28 +0530

    Demo_first_Git

---
+++ gittest/src/main/java/Guru99/gittest/App.java
@@ -0,0 +1,13 @@
+package Guru99.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gittest/src/test/java/Guru99/gittest/AppTest.java
@@ -0,0 +1,33 @@
+package Guru99.gittest;
+
+import java.io.FileNotFoundException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest {
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "E://selenium//chromedriver//chromedriver.exe");
+		driver = new ChromeDriver();
+		String baseUrl = "http://newtours.demoaut.com";
+		String expectedTitle = "Welcome: Mercury Tours";
+		String actualTitle = "";
+
+		// launch Fire fox and direct it to the Base URL
+		driver.get(baseUrl);
+
+		// get the actual value of the title
+		actualTitle = driver.getTitle();
+		if (actualTitle.contentEquals(expectedTitle)) {
+			System.out.println("Test Passed!");
+		} else {
+			System.out.println("Test Failed");
+		}
+
+	}
+}
