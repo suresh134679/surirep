0bec05b32d5525ed6bfa32179be6a79b1d133592
suresh134679 <suresh134679@gmail.com>
2017-06-07 07:37:59 +0530

    adding line from git for Jenkins Trigger test

--- gittest/src/main/java/Guru99/gittest/App.java
+++ gittest/src/main/java/Guru99/gittest/App.java
@@ -10,6 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "bye..." );
+        System.out.println( "...adding line from git.." );
         
        
     }
